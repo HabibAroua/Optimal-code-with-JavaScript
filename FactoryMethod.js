4a0f5e22913119016c58462ead9f14d5a9409c61
HabibAroua <habibha.aroua82@gmail.com>
2020-09-04 22:06:38 +0100

    show the result

--- FactoryMethod.js
+++ FactoryMethod.js
@@ -9,16 +9,23 @@ class BmwFactory
     
     create(type)
     {
-        if(type == "X5")
+        if((type == "X5") || (type == "x5"))
         {
-            return BmwFactory(type,108000,300);
+            return new BmwFactory(type,108000,300);
         }
         else
         {
-            if(type == "X6")
+            if((type == "X6") || (type == "x6"))
             {
-                return BmwFactory(type,111000,320);
+                return new BmwFactory(type,111000,320);
             }
         }
     }
 }
+
+const bmw = new BmwFactory();
+const x5 = bmw.create('X5');
+const x6 = bmw.create('X6');
+
+console.log(x5);
+console.log(x6);
