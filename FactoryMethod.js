9c9a42204912407da7423569bc41aaaef5462868
HabibAroua <habibha.aroua82@gmail.com>
2020-09-04 22:00:42 +0100

    dev create method

--- FactoryMethod.js
+++ FactoryMethod.js
@@ -6,4 +6,19 @@ class BmwFactory
         this.price=price;
         this.maxSpeed=maxSpeed;
     }
+    
+    create(type)
+    {
+        if(type == "X5")
+        {
+            return BmwFactory(type,108000,300);
+        }
+        else
+        {
+            if(type == "X6")
+            {
+                return BmwFactory(type,111000,320);
+            }
+        }
+    }
 }
