02b3bcd4420cfb2b7d8c4445bed0c7680f4e9eba
HabibAroua <habibha.aroua82@gmail.com>
2020-09-04 21:48:37 +0100

    show the result

--- Singleton.js
+++ Singleton.js
@@ -22,4 +22,15 @@ class Counter
     }
 }
 
+const myCount1 = new Counter();
+const myCount2 = new Counter();
+
+myCount1.increaseCounter();
+myCount1.increaseCounter();
+myCount2.increaseCounter();
+myCount2.increaseCounter();
+
+console.log("the number is "+myCount1.getCounter());
+console.log("the number is "+myCount2.getCounter());
+
 //Link of tuto is https://www.youtube.com/watch?v=GrYs0qDQEp0&list=PLNkWIWHIRwMGzgvuPRFkDrpAygvdKJIE4&index=2
