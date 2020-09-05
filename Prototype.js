7ba2e34992309e295a1d3d1ec8400f4f17f4295a
HabibAroua <habibha.aroua82@gmail.com>
2020-09-05 16:29:00 +0100

    show the result

--- Prototype.js
+++ Prototype.js
@@ -13,3 +13,20 @@ class TeslaCar
         return new TeslaCar(this.model,this.price,this.interior,this.autopilot);
     }
 }
+
+//Produce base auto
+const protoTypeCar = new  TeslaCar('S',8000,'black',false);
+
+//Cloning of base auto
+const car1 = protoTypeCar.produce();
+const car2 = protoTypeCar.produce();
+const car3 = protoTypeCar.produce();
+
+//Change the particular auto
+car1.interior = "white";
+car1.autopilot = true;
+
+//Show the result
+console.log(car1);
+console.log(car2);
+console.log(car3);
