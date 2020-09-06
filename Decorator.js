800c3ce14b04276ef75048756a8a44443540122b
HabibAroua <habibha.aroua82@gmail.com>
2020-09-06 18:02:11 +0100

    Add new class named Car

---
+++ Decorator.js
@@ -0,0 +1,18 @@
+class Car
+{
+    constructor()
+    {
+        this.price = 1000000;
+        this.model = 'Car';
+    }
+    
+    getPrice()
+    {
+        return this.price;
+    }
+    
+    getDescription()
+    {
+        return this.model;
+    }
+}
