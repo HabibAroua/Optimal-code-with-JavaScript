4311b8282339eb9691b6e27a1935f9f87a1b7c3a
HabibAroua <habibha.aroua82@gmail.com>
2020-09-08 01:21:26 +0100

    add class for proxy

--- proxy.js
+++ proxy.js
@@ -10,3 +10,33 @@ class CarAccess
         console.log('Closing the car door');
     }
 }
+
+class SecuritySystem
+{
+    constructor(door)
+    {
+        this.door = door;
+    }
+    
+    open(password)
+    {
+        if(this.authenticate(password))
+        {
+            this.door.open();
+        }
+        else
+        {
+            console.log('Access denied!');
+        }
+    }
+    
+    authenticate(password)
+    {
+        return password == 'Ilon';
+    }
+    
+    close()
+    {
+        this.door.close();
+    }
+}
