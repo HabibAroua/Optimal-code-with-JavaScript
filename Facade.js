ab433d4fbe901a5db83defb38b74a2622bddbe44
HabibAroua <habibha.aroua82@gmail.com>
2020-09-08 01:00:21 +0100

    add new class

---
+++ Facade.js
@@ -0,0 +1,47 @@
+class Convetor
+{
+    setBody()
+    {
+        console.log('Body set !');
+    }
+    
+    getEngine()
+    {
+        console.log('Dismant Engine !');
+    }
+    
+    setEngine()
+    {
+        console.log('Engine set !');
+    }
+    
+    setInterior()
+    {
+        console.log('Exterior added!');
+    }
+    
+    getInterior()
+    {
+        console.log('Update interior');
+    }
+    
+    setExterior()
+    {
+        console.log('Added interior');
+    }
+    
+    setWheels()
+    {
+        console.log('Wheels!');
+    }
+    
+    addElectronics()
+    {
+        console.log('Added electronics!');
+    }
+    
+    paint()
+    {
+        console.log('Car painted!');
+    }
+}
