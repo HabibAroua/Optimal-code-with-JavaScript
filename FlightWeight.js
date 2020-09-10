7c831b7c2882dfd3ce2ac3717b9d6cce69ad1a1e
HabibAroua <habibha.aroua82@gmail.com>
2020-09-10 23:35:13 +0100

    create and add flighwight

---
+++ FlightWeight.js
@@ -0,0 +1,24 @@
+class Auto
+{
+    constructor(model)
+    {
+        this.model = model;
+    }
+}
+
+class AutoFactory
+{
+    constructor(name)
+    {
+        this.models = {};
+    }
+    
+    create(name)
+    {
+        let model = this.models[name];
+        if (model) return model;
+        console.count('model');
+        this.models[name] = new Auto(name);
+        return this.models[name];
+    }
+}
