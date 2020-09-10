3360315e7247728be3c209504d81e7963019705f
HabibAroua <habibha.aroua82@gmail.com>
2020-09-10 22:13:45 +0100

    add two classes in Adpater .js

---
+++ Adapter.js
@@ -0,0 +1,15 @@
+class Engine2
+{
+    simpleInterface()
+    {
+        console.log('Engine 2.0 - tr-tr-tr');
+    }
+}
+
+class EngineV8
+{
+    complecatedInterface()
+    {
+        console.log('Engine V8! - wroom wroom!');
+    }
+}
