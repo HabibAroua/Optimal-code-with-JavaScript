4256e4b4478bdf4d5d14833d0671f4024063a9ce
HabibAroua <habibha.aroua82@gmail.com>
2020-09-10 22:50:04 +0100

    finish the composite

--- Composite.js
+++ Composite.js
@@ -50,3 +50,41 @@ class Tools extends Equipment
         this.setPrice(4000);
     }
 }
+
+class Composite extends Equipment
+{
+    constructor()
+    {
+        super();
+        this.equipments = [];
+    }
+    
+    add(equipment)
+    {
+        this.equipments.push(equipment);
+    }
+    
+    getPrice()
+    {
+        return this.equipments
+                .map(equipment => equipment.getPrice())
+                .reduce((a,b) => a+b);
+    }
+}
+
+class Car extends Composite
+{
+    constructor()
+    {
+        super();
+        this.setName('Audi');
+    }
+}
+
+const myCar = new Car();
+
+myCar.add(new Engine());
+myCar.add(new Body());
+myCar.add(new Tools());
+
+console.log(`${myCar.getName()} price is ${myCar.getPrice()}$`);
