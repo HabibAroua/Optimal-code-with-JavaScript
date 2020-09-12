e3c001b25bab2859f3dafa8979ce222157300243
HabibAroua <habibha.aroua82@gmail.com>
2020-09-12 15:52:19 +0100

    finish iterator

---
+++ Iterator.js
@@ -0,0 +1,25 @@
+class Iterator
+{
+    constructor(el)
+    {
+        this.index = 0;
+        this.elements = el;
+    }
+    
+    next()
+    {
+        return this.elements[this.index++];
+    }
+    
+    hasNext()
+    {
+        return this.index < this.elements.length;
+    }
+}
+
+const collection = new Iterator(['Audi','BMW','Tesla','Mersedes']);
+
+while(collection.hasNext())
+{
+    console.log(collection.next());
+}
