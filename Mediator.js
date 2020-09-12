e1fbbd1f620cf844517eb108c2ace7b9fc850437
HabibAroua <habibha.aroua82@gmail.com>
2020-09-12 15:38:11 +0100

    create new class

--- Mediator.js
+++ Mediator.js
@@ -24,3 +24,22 @@ class OfficialDealer
         return this.customers;
     }
 }
+
+class Customer
+{
+    constructor(name, dealerMediator)
+    {
+        this.name = name;
+        this.dealerMediator = dealerMediator;
+    }
+    
+    getName()
+    {
+        return this.name;
+    }
+    
+    makeOrder(auto, info)
+    {
+        this.dealerMediator.orderAuto(this, auto, info);
+    }
+}
