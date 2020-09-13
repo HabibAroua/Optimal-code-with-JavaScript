02f8073499763af914bf7b448270bd6d89d47c1e
HabibAroua <habibha.aroua82@gmail.com>
2020-09-13 23:08:58 +0100

    show all results

--- Strategy.js
+++ Strategy.js
@@ -35,3 +35,12 @@ class AutoCart
 const baseCustomer = new AutoCart(baseStrategy);
 const premiumCustomer = new AutoCart(premiumStrategy);
 const platinumCustomer = new AutoCart(platinumStrategy);
+
+baseCustomer.setAmount(50000);
+console.log(baseCustomer.checkout());
+
+premiumCustomer.setAmount(50000);
+console.log(premiumCustomer.checkout());
+
+platinumCustomer.setAmount(50000);
+console.log(platinumCustomer.checkout());
